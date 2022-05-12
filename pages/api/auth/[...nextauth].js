b9e55ce01d383f76e6771884c886cc4fa651f1f0
watarunakanishi190647 <95616173+watarunakanishi190647@users.noreply.github.com>
2022-05-12 15:56:43 +0900

    小文字に直した

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/Google"
+import GoogleProvider from "next-auth/providers/google"
 
 export default NextAuth({
   // Configure one or more authentication providers
